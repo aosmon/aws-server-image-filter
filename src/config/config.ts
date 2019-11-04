e6ea74c141577e4a6e7cfe073d627a7e5d8aff1f
Aizhan Osmonova <osmonova86@gmail.com>
2019-11-04 16:47:32 -0500

    Update aws config env variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,10 @@
 export const config = {
-    "dev": {
-      "aws_region": "us-east-1",
-      "aws_profile": "default"
-    },
-    "prod": {
-    }
+  "dev": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+  },
+  "prod": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
   }
-  
+}
